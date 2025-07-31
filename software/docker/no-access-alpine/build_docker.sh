7825d58677bdf0cd69628760aa9e117f264fd17d
Aaron Krawczyk <aaron.krawczyk@gmail.com>
2025-07-31 11:36:47 +0100

    Update build_docker.sh

--- software/docker/no-access-alpine/build_docker.sh
+++ software/docker/no-access-alpine/build_docker.sh
@@ -1,3 +1,1 @@
-# openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout nginx-selfsigned.key -out nginx-selfsigned.crt -subj "/CN=*.web-rated.ie"
-# px5g selfsigned -days 365 -newkey rsa:2048 -keyout nginx-selfsigned.key -out nginx-selfsigned.crt -subj "/CN=*.web-rated.ie"
 docker build -t webbound:v1.0 .
